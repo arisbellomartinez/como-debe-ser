8ebfc0b6286596c77efceb20d243c6bd987f96d5
manuelgardon <manugardon801@gmail.com>
2023-09-07 19:20:52 -0300

    Paciente controller

---
+++ src/main/java/com/dh/clinicaOdontologica/controller/PacienteController.java
@@ -0,0 +1,48 @@
+package com.dh.clinicaOdontologica.controller;
+
+import com.dh.clinicaOdontologica.entity.dto.OdontologoDTO;
+import com.dh.clinicaOdontologica.entity.dto.PacienteDTO;
+import com.dh.clinicaOdontologica.service.IPacienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping("/pacientes")
+public class PacienteController {
+
+    @Autowired
+    IPacienteService pacienteService;
+
+    @PostMapping()
+    public ResponseEntity<?> crearPaciente(@RequestBody PacienteDTO pacienteDTO){
+        pacienteService.agregarPaciente(pacienteDTO);
+        return ResponseEntity.status(HttpStatus.OK).body("Paciente creado");
+    }
+
+    @GetMapping("/listarTodos")
+    public Set<PacienteDTO> listarPacientes() {
+        return pacienteService.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public PacienteDTO listarPacientePorId(@PathVariable Long id){
+        return pacienteService.listarPaciente(id);
+    }
+
+    @PutMapping()
+    public ResponseEntity<String> actualizarPaciente(@RequestBody PacienteDTO pacienteDTO){
+        pacienteService.modificarPaciente(pacienteDTO);
+        return ResponseEntity.status(HttpStatus.OK).body("Paciente actualizado");
+    }
+
+    @DeleteMapping("eliminar/{id}")
+    public ResponseEntity<?> eliminarPaciente(@PathVariable("id") Long id){
+        pacienteService.eliminarPaciente(id);
+        return ResponseEntity.status(HttpStatus.OK).body("Paciente eliminado");
+    }
+
+}
